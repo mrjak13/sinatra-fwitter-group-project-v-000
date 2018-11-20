5fa9223610997981cd3917e629ddb0cf081b31c2
Justin Kenney <justinkenney13@gmail.com>
2018-11-20 21:45:19 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,32 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  get '/signup' do
+    erb :'/users/signup'
+  end
 
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    elsif User.find_by(username: params[:username])
+      @user = User.find_by(username: params[:username])
+      redirect to '/login'
+    else
+      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+    end
+    redirect to '/tweets'
+  end
+
+  get '/login' do
+    erb :'/login'
+  end
+
+  post '/login' do
+    if params[:username] == "" || params[:password] == ""
+      redirect to '/login'
+    else
+    end
+    redirect to '/tweets'
+  end
 
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,48 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    erb :'/tweets/index'
+  end
+
+  get '/tweets/new' do
+    erb :'/tweets/new'
+  end
+
+  post '/tweets' do
+    if params[:content] == ""
+      redirect to '/tweets/new'
+    else
+      @tweet = Tweet.create(content: params[:content])
+    end
+    redirect to "/tweets/#{@tweet.id}"
+  end
+
+  get '/tweets/:id' do
+    @tweet = Tweet.find(params[:id])
+    erb :'/tweets/show'
+  end
+
+  get '/tweets/:id/edit' do
+    @tweet = Tweet.find(params[:id])
+    erb :'/tweets/edit'
+  end
+
+  patch '/tweets/:id/edit' do
+
+    @tweet = Tweet.find(params[:id])
+    if params[:content] == ""
+      redirect to "/tweets/#{@tweet.id}"
+    else
+    @tweet.update(content: params[:content])
+    end
+    redirect to "/tweets/#{@tweet.id}"
+  end
+
+  delete '/tweets/:id' do
+    @tweet = Tweet.find(params[:id])
+    @tweet.delete
+
+    redirect to 'tweets/index'
+  end
 
 end
