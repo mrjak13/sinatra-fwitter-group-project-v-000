a996bee36c00bd640f9bcfceee9e326cb2674af0
Justin Kenney <justinkenney13@gmail.com>
2018-11-21 20:18:18 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,31 @@
 class UsersController < ApplicationController
 
-  # get '/users/signup' do
-  #   erb :'/users/signup'
-  # end
-  #
-  # post '/users/signup' do
-  #   if params[:username] == "" || params[:email] == "" || params[:password] == ""
-  #     redirect to '/users/signup'
-  #   elsif User.find_by(username: params[:username])
-  #     @user = User.find_by(username: params[:username])
-  #     redirect to '/users/login'
-  #   else
-  #     @user = User.create(username: params[:username], email: params[:email], password: params[:password])
-  #     session[:user_id] = @user.id
-  #   end
-  #   redirect to '/tweets'
-  # end
-  #
-  # get '/users/login' do
-  #   erb :'/users/login'
-  # end
-  #
-  # post '/users/login' do
-  #   binding.pry
-  #   if params[:username] == "" || params[:password] == ""
-  #     redirect to '/users/login'
-  #   elsif params[:username]
-  #   end
-  #   redirect to '/tweets'
-  # end
-  #
-  # get '/users/logout' do
-  #   session.clear
-  # end
+  get '/signup' do
+    erb :'/users/signup'
+  end
 
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    elsif User.find_by(username: params[:username])
+      @user = User.find_by(username: params[:username])
+      redirect to '/login'
+    else
+      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+      session[:user_id] = @user.id
+    end
+    redirect to '/tweets'
+  end
+
+  get '/login' do
+    erb :'/login'
+  end
+
+  post '/login' do
+    if params[:username] == "" || params[:password] == ""
+      redirect to '/login'
+    else
+    end
+    redirect to '/tweets'
+  end
 end
