beeaf71bf9bcb3e5e1b80ba5eb6f159b3e003f4d
Justin Kenney <justinkenney13@gmail.com>
2018-11-21 20:49:02 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
     end
     binding.pry
-    redirect to '/tweets'
+    erb '/tweets'
   end
 
   get '/login' do
