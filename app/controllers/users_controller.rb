a2a275d9ffc4591e65b815c68ec61ee6b56d725c
Justin Kenney <justinkenney13@gmail.com>
2018-11-21 20:05:15 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,37 @@
 class UsersController < ApplicationController
 
+  get '/users/signup' do
+    erb :'/users/signup'
+  end
+
+  post '/users/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    elsif User.find_by(username: params[:username])
+      @user = User.find_by(username: params[:username])
+      redirect to '/login'
+    else
+      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+      session[:user_id] = @user.id
+    end
+    redirect to '/tweets'
+  end
+
+  get '/users/login' do
+    erb :'/login'
+  end
+
+  post '/users/login' do
+    if params[:username] == "" || params[:password] == ""
+      redirect to '/login'
+    else
+    end
+    redirect to '/tweets'
+  end
+
+  get '/users/logout' do
+    session.clear
+  end
+
 
 end
