e11360290d60abc4996b45b3824439e86e9a859c
Justin Kenney <justinkenney13@gmail.com>
2018-11-21 20:18:34 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,33 +13,41 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get '/signup' do
-    erb :'/users/signup'
+  # get '/signup' do
+  #   erb :'/users/signup'
+  # end
+  #
+  # post '/signup' do
+  #   if params[:username] == "" || params[:email] == "" || params[:password] == ""
+  #     redirect to '/signup'
+  #   elsif User.find_by(username: params[:username])
+  #     @user = User.find_by(username: params[:username])
+  #     redirect to '/login'
+  #   else
+  #     @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+  #     session[:user_id] = @user.id
+  #   end
+  #   redirect to '/tweets'
+  # end
+  #
+  # get '/login' do
+  #   erb :'/login'
+  # end
+  #
+  # post '/login' do
+  #   if params[:username] == "" || params[:password] == ""
+  #     redirect to '/login'
+  #   else
+  #   end
+  #   redirect to '/tweets'
+  # end
+
+  def current_user
+
   end
 
-  post '/signup' do
-    if params[:username] == "" || params[:email] == "" || params[:password] == ""
-      redirect to '/signup'
-    elsif User.find_by(username: params[:username])
-      @user = User.find_by(username: params[:username])
-      redirect to '/login'
-    else
-      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
-      session[:user_id] = @user.id
-    end
-    redirect to '/tweets'
-  end
-
-  get '/login' do
-    erb :'/login'
-  end
-
-  post '/login' do
-    if params[:username] == "" || params[:password] == ""
-      redirect to '/login'
-    else
-    end
-    redirect to '/tweets'
+  def logged_in?
+    
   end
 
 end
