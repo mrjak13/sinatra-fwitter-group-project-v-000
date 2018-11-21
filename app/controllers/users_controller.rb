5993e720647ed2fadd8d84f01255ec672e187f56
Justin Kenney <justinkenney13@gmail.com>
2018-11-21 21:40:17 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,6 +47,8 @@ class UsersController < ApplicationController
   end
 
   get '/users/:slug' do
-    binding.pry
+    @user = User.find_by_slug(params[:slug])
+
+    erb :'/users/show'    
   end
 end
