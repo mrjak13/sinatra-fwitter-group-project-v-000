7947d91684cf181572147bf4628ca3c95b9d7358
Justin Kenney <justinkenney13@gmail.com>
2018-11-21 20:13:23 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,37 @@
 class UsersController < ApplicationController
 
-  get '/users/signup' do
-    erb :'/users/signup'
-  end
-
-  post '/users/signup' do
-    if params[:username] == "" || params[:email] == "" || params[:password] == ""
-      redirect to '/users/signup'
-    elsif User.find_by(username: params[:username])
-      @user = User.find_by(username: params[:username])
-      redirect to '/users/login'
-    else
-      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
-      session[:user_id] = @user.id
-    end
-    redirect to '/tweets'
-  end
-
-  get '/users/login' do
-    erb :'/users/login'
-  end
-
-  post '/users/login' do
-    binding.pry
-    if params[:username] == "" || params[:password] == ""
-      redirect to '/users/login'
-    elsif params[:username]
-    end
-    redirect to '/tweets'
-  end
-
-  get '/users/logout' do
-    session.clear
-  end
+  # get '/users/signup' do
+  #   erb :'/users/signup'
+  # end
+  # 
+  # post '/users/signup' do
+  #   if params[:username] == "" || params[:email] == "" || params[:password] == ""
+  #     redirect to '/users/signup'
+  #   elsif User.find_by(username: params[:username])
+  #     @user = User.find_by(username: params[:username])
+  #     redirect to '/users/login'
+  #   else
+  #     @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+  #     session[:user_id] = @user.id
+  #   end
+  #   redirect to '/tweets'
+  # end
+  # 
+  # get '/users/login' do
+  #   erb :'/users/login'
+  # end
+  # 
+  # post '/users/login' do
+  #   binding.pry
+  #   if params[:username] == "" || params[:password] == ""
+  #     redirect to '/users/login'
+  #   elsif params[:username]
+  #   end
+  #   redirect to '/tweets'
+  # end
+  # 
+  # get '/users/logout' do
+  #   session.clear
+  # end
 
 end
